1166e8b31ced38ff39faa8a1a561648707585a09
Andreas Grimme <andreas.grimme@siemens-healthineers.com>
2022-03-03 10:40:54 +0100

    task

--- Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// Write a program that finds all words that have a meaning when spelled backwards.
-// List of English words is available in words.txt
-
-Console.WriteLine("Hello, World!");
---
+++ Readme.md
@@ -0,0 +1,6 @@
+﻿# Palindrome Code Kata 
+
+Write a program that finds all words that have a meaning when spelled backwards.
+
+A list of English words is provided in words.txt
+
